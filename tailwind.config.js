1bc616d407cc4ab2c0bc84be61eda1ad4addfb08
neopromic <cusaowesley1@gmail.com>
2023-07-20 07:54:31 -0300

    FEAT: flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'primary': '#D0BCFF',
+      'onPrimary': '#381E72',
+      'secondary': '#CCC2DC',
+    },
     extend: {
       
     },
