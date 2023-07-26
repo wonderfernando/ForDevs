af1b249667ee53c2f3488458ef4a4ad28da4c63a
neopromic <cusaowesley1@gmail.com>
2023-07-26 17:32:44 -0300

    FEAT: working with typescript (test)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 import * as dotenv from 'dotenv';
 dotenv.config()
-import express from 'express'
-const app = express()
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-
-const __dirname = path.dirname(__filename);
-
-
-app.get("/", (req,res) =>{
-    res.sendFile(__dirname + "/")
-})
-
-app.listen(8081);
